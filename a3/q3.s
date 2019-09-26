4bc7c10e32c50c9ae0383c301c6bafbde4853043
Mohamed <b7e9@ugrad.cs.ubc.ca>
2019-09-26 13:24:03 -0700

    fix b3

--- a3/q3.s
+++ a3/q3.s
@@ -23,19 +23,20 @@ st r3, 0(r2)            #p = r3
 
 #p++ p now points to the next integer in array --> b[1]
 ld 0(r2), r4            #r4 = value of p
-inca r4                 #r4 = r4 + 4 
+inca r4                  #r4 = r4 + 1
 st r4, 0(r2)            #p = r4
 
 #p[a] = b[a];
 ld 0(r0), r4            #r4 = a = 2
-ld (r3, r4, 4), r4      #r4 = b[a] = b[2] = 2
+ld (r3, r4, 4), r7      #r4 = b[a] = b[2] = 2
 
 ld 0(r2), r6            #r6 = address p is pointing to
 
-st r4, (r6, r4, 4)      #p[a] = r4 (p[2] = b[1] = 2)
+st r7, (r6, r4, 4)      #p[a] = r4 (p[2] = b[1] = 2)
 
 # *(p+3) = b[0]   
-ld 0(r3), r5            #r5 = b[0] = 0 
+ld 0(r3), r5            #r5 = b[0] = 0
+ld $p, r2				#r2 = address of p
 ld 0(r2), r6            #r6 = value of p
 st r5, 12(r6)           #p[3] = r5
 
